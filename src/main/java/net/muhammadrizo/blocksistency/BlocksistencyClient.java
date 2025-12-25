87097de90619b1fccd8c3d1bd89e7b74230a9d3d
Muhammadrizo <muhammadrizo.dev@gmail.com>
2025-12-25 09:56:36 +0500

    Initial commit

---
+++ src/main/java/net/muhammadrizo/blocksistency/BlocksistencyClient.java
@@ -0,0 +1,10 @@
+package net.muhammadrizo.blocksistency;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlocksistencyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/muhammadrizo/blocksistency/BlocksistencyDataGenerator.java
@@ -0,0 +1,11 @@
+package net.muhammadrizo.blocksistency;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BlocksistencyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}

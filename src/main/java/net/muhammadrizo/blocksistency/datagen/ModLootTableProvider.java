c75305b0083a6d414a82dad3e5c1717086773f12
Muhammadrizo <muhammadrizo.dev@gmail.com>
2026-01-19 22:18:46 +0500

    Fix: Double slabs now correctly drop 2 items

--- src/main/java/net/muhammadrizo/blocksistency/datagen/ModLootTableProvider.java
+++ src/main/java/net/muhammadrizo/blocksistency/datagen/ModLootTableProvider.java
@@ -26,29 +26,29 @@ public class ModLootTableProvider extends FabricBlockLootTableProvider {
 
         addDrop(ModStoneBlocks.POLISHED_STONE);
         addDrop(ModStoneBlocks.POLISHED_STONE_STAIRS);
-        addDrop(ModStoneBlocks.POLISHED_STONE_SLAB);
+        addDrop(ModStoneBlocks.POLISHED_STONE_SLAB, slabDrops(ModStoneBlocks.POLISHED_STONE_SLAB));
         addDrop(ModStoneBlocks.POLISHED_STONE_WALL);
 
         addDrop(ModStoneBlocks.MOSSY_CHISELED_STONE_BRICKS);
 
         addDrop(ModStoneBlocks.CRACKED_STONE_BRICK_STAIRS);
-        addDrop(ModStoneBlocks.CRACKED_STONE_BRICK_SLAB);
+        addDrop(ModStoneBlocks.CRACKED_STONE_BRICK_SLAB, slabDrops(ModStoneBlocks.CRACKED_STONE_BRICK_SLAB));
         addDrop(ModStoneBlocks.CRACKED_STONE_BRICK_WALL);
         addDrop(ModStoneBlocks.CRACKED_CHISELED_STONE_BRICKS);
 
         addDrop(ModStoneBlocks.STONE_TILES);
         addDrop(ModStoneBlocks.STONE_TILE_STAIRS);
-        addDrop(ModStoneBlocks.STONE_TILE_SLAB);
+        addDrop(ModStoneBlocks.STONE_TILE_SLAB, slabDrops(ModStoneBlocks.STONE_TILE_SLAB));
         addDrop(ModStoneBlocks.STONE_TILE_WALL);
 
         addDrop(ModStoneBlocks.MOSSY_STONE_TILES);
         addDrop(ModStoneBlocks.MOSSY_STONE_TILE_STAIRS);
-        addDrop(ModStoneBlocks.MOSSY_STONE_TILE_SLAB);
+        addDrop(ModStoneBlocks.MOSSY_STONE_TILE_SLAB, slabDrops(ModStoneBlocks.MOSSY_STONE_TILE_SLAB));
         addDrop(ModStoneBlocks.MOSSY_STONE_TILE_WALL);
 
         addDrop(ModStoneBlocks.CRACKED_STONE_TILES);
         addDrop(ModStoneBlocks.CRACKED_STONE_TILE_STAIRS);
-        addDrop(ModStoneBlocks.CRACKED_STONE_TILE_SLAB);
+        addDrop(ModStoneBlocks.CRACKED_STONE_TILE_SLAB, slabDrops(ModStoneBlocks.CRACKED_STONE_TILE_SLAB));
         addDrop(ModStoneBlocks.CRACKED_STONE_TILE_WALL);
 
         addDrop(ModStoneBlocks.STONE_PILLAR);
